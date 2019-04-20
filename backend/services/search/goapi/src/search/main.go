f21b8272c8910f8c865258a61618c26902878d76
Mayur Barge <bargemayur05@gmail.com>
2019-04-19 23:26:37 -0700

    Adding initial file for search API

---
+++ backend/services/search/goapi/src/search/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	http "net/http"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+
+//Test Status of the API
+func PingEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Search API is alive!")
+}
+
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/ping", PingEndPoint).Methods("GET")
+	
+	if err := http.ListenAndServe(":3000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(r)); err != nil {
+		log.Fatal(err)
+	}
+}
