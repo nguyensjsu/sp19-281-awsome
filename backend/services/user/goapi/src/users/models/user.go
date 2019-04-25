22eec838c7460d1e372e845c6dd313dcdb99aa96
Dharma Dheeraj Chintala <38597753+dharmadheeraj@users.noreply.github.com>
2019-04-24 17:48:37 -0700

    Front end functionality check

--- backend/services/user/goapi/src/users/models/user.go
+++ backend/services/user/goapi/src/users/models/user.go
@@ -1,10 +1,14 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
-
 type User struct {
-	ID  bson.ObjectId `bson:"_id" json:"id"`
+	//ID  bson.ObjectId `bson:"_id" json:"id"`
 	Email string        `bson:"email" json:"email"`
 	FirstName string	`bson:"firstName" json:"firstName"`
 	LastName string	`bson:"lastName" json:"lastName"`
+	Profile string	`bson:"profile" json:"profile"`
+}
+
+type Login struct {
+	Email string        `bson:"email" json:"email"`
+	Password string	`bson:"password" json:"password"`
 }
