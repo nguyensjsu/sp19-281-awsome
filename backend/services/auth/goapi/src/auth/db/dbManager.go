503bfc1cc69f8e316dd45ebbb987003efe782341
nikitabairagi <nikitabairagi@gmail.com>
2019-04-29 01:42:17 -0700

    db implementation in auth service

--- backend/services/auth/goapi/src/auth/db/dbManager.go
+++ backend/services/auth/goapi/src/auth/db/dbManager.go
@@ -1,59 +1,114 @@
-// mocked db impl
-// todo connect to db
 package db
 
 import (
 	"../models"
+	"context"
+	"fmt"
 	uuid "github.com/satori/go.uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
 )
 
-// user password map
-var users = map[string]string{}
+var db *mongo.Database = nil
 
-// token to user map
-// todo this should be implemented with ttl
-var activeTokens = map[uuid.UUID]string{}
+func ConfigMongoDB() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	//defer client.Disconnect(ctx)
+
+	db = client.Database("authdb")
+
+	// create Time To Live index
+	//coll := db.Collection("active_sessions")
+	//opts := options.CreateIndexes().SetMaxTime(10 * time.Second)
+	//keys := bsonx.Doc({"createdAt": 1}, { expireAfterSeconds: 60 * 60})
+	//index := mongo.IndexModel{}
+	//index.Keys = keys
+	//_, err = coll.Indexes().CreateOne(context.Background(), index, opts)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+}
+
+func AddUser(user models.User) bool {
+	// check if user already exists
+	if _, userExist := GetUserByEmail(user.Email); userExist {
+		return false
+	}
+
+	coll := db.Collection("users")
+	_, err := coll.InsertOne(context.Background(),user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return true
+}
 
 func GetUserByEmail(email string) (models.User, bool) {
-	hashedPassword, exist := users[email]
-	return models.User{Email: email, Password: hashedPassword}, exist
-}
-
-func AddUser(user models.User) bool{
-	//check if user exists
-	_, userExist := users[user.Email]
-	if userExist {
-		return false
-	} else {
-		users[user.Email] = user.Password
-		return true
+	var user models.User
+	userExist := true
+	coll := db.Collection("users")
+	err := coll.FindOne(context.Background(), bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		userExist = false
 	}
+	return user, userExist
 }
 
-func AddToken(email string) string {
+func AddToken(user models.User) string {
 	newToken, _ := uuid.NewV4()
-	activeTokens[newToken] = email
+	coll := db.Collection("active_sessions")
+	session := models.Session{
+		CreatedAt: time.Now(),
+		SessionId: newToken.String(),
+		Email: user.Email,
+		Role: user.Role }
+	_, err := coll.InsertOne(context.Background(), session)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return newToken.String()
 }
 
-func GetToken(token string) (string, bool) {
-	uuidToken, ok := uuid.FromString(token)
-	if ok != nil {
-		// invalid format token
-		panic("invalid token")
+func GetToken(token string) (models.Session, bool) {
+	var session models.Session
+	sessionExist := true
+	coll := db.Collection("active_sessions")
+	err := coll.FindOne(context.Background(), bson.M{"sessionId": token}).Decode(&session)
+	if err != nil {
+		sessionExist = false
 	}
-	email, tokenExist := activeTokens[uuidToken]
-	return email, tokenExist
+	return session, sessionExist
 }
 
 func DeleteToken(token string)  {
-	uuidToken, ok := uuid.FromString(token)
-	if ok != nil {
-		// invalid format token
-		panic("invalid token")
-	}
-	_, tokenExist := activeTokens[uuidToken]
-	if tokenExist {
-		delete(activeTokens, uuidToken)
+	coll := db.Collection("active_sessions")
+	_, err := coll.DeleteOne(context.Background(), bson.M{"sessionId": token})
+	if err != nil {
+		log.Fatal(err)
 	}
 }
