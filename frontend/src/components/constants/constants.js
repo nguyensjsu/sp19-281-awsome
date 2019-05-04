8e99821c0c16c93c45c2828803b931b43cd44ce1
AvinavTyagi <avinav.tyagi@sjsu.edu>
2019-05-03 23:13:05 -0700

    constantjs update

--- frontend/src/components/constants/constants.js
+++ frontend/src/components/constants/constants.js
@@ -6,3 +6,4 @@ export const USER_API = "https://dxj7ycaazh.execute-api.us-east-1.amazonaws.com/
 export const SEARCH_API = "http://localhost:9000" //serach api to be given
 
 export const JOB_API = "https://dxj7ycaazh.execute-api.us-east-1.amazonaws.com/prod/jobs" //job api to be given
+//export const JOB_API = "http://localhost:9000/jobs"
